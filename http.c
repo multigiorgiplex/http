538a9a87293a462870be1a317c824aeafc4de3cb
multigiorgiplex_portatile <marco@portatile_arch.localdomain>
2018-08-22 22:35:42 +0200

    Added files

---
+++ http.c
@@ -0,0 +1,212 @@
+#include "http.h"
+
+void http_initialize (int debug)
+{
+	if (debug >= 0 && debug <= 2)
+	{
+		_http_debug = debug;
+		_http_flag_init = 1;
+
+		printf ("[" __FILE__ "] i! libhttp.so inizializzata (debug level: %d)\n", debug);
+	}
+	else
+	{
+		fprintf (stderr, "[" __FILE__ ":%u] E! Livello debug non in range\n", __LINE__);
+	}
+}
+
+int http_parse_request (char *r, char *t, char *f, char *a)
+{
+	char *split;
+	char *split2;
+
+	char *cpy;
+	char *str;
+
+	if (!_http_flag_init)
+	{
+		fprintf (stderr, "[" __FILE__ ":%u] E! Libreria non inizializzata\n", __LINE__);
+		return -1;
+	}
+	
+	//Azzero i buffer
+	memset (t, 0, 5);
+	memset (f, 0, HTTP_REQUEST_FILE_LEN);
+	memset (a, 0, 100);	
+	
+	if (_http_debug >= 2) printf ("[" __FILE__ "] i! HTTP Header:\n%s\n", r);
+
+	split = strchr (r, '\r');
+	if (split == NULL)
+	{
+		fprintf (stderr, "[" __FILE__ ":%u] E! Request invalida! [E-1]\n", __LINE__);
+		return -1;
+	}
+	*split = 0;
+	
+	if (_http_debug) printf ("[" __FILE__ "] i! HTTP Request: %s\n", r);	// r = "GET /file.asd HTTP/1.1"
+
+		// Trova il primo spazio e lo sostituisce con un fine stringa
+		// 		"GET /file.asd HTTP/1.1"
+		//			↑	
+		split2 = strchr (r, ' ');
+		if (split2 == NULL)
+		{
+			fprintf (stderr, "[" __FILE__ ":%u] E! Request invalida! [E-2]\n", __LINE__);
+			return -2;
+		}
+		*split2 = 0;
+
+		// Copia la stringa in t fino al fine stringa
+		cpy = memccpy (t, r, 0, 5);
+		if (cpy == NULL)
+		{
+			fprintf (stderr, "[" __FILE__ ":%u] E! Request invalida! [E-3]\n", __LINE__);
+			return -3;
+		}
+
+		// Copia in f dal carattere dopo il fine stringa fino allo spazio
+		// 		"GET /file.asd HTTP/1.1"
+		//			 →		  ↑	
+		cpy = memccpy (f, split2+1, ' ', HTTP_REQUEST_FILE_LEN);
+		if (cpy == NULL)
+		{
+			fprintf (stderr, "[" __FILE__ ":%u] E! Request invalida! [E-4]\n", __LINE__);
+			return -4;
+		}
+		*(cpy-1) = 0;
+	
+	//Controllo se ci sono argomenti di tipo GET
+	split2 = strchr (f, '?');
+	if (split2 != NULL)	//Ci sono argomenti GET
+	{
+		memccpy (a, split2+1, 0, 100);
+		memset (split2, 0, strlen (split2));	//pulisco il buffer file
+	}
+
+	if (!memcmp (t, "GET\0", 5))	// Se e' una richiesta GET
+	{
+		;
+	}
+	else if (!memcmp (t, "POST\0", 5))	// Se e' una richiesta GET
+	{
+		//	ANALIZZO IL RESTO DELL'HEADER REQUEST
+		while (split != NULL)
+		{
+			str = split+2;
+			split = strchr (str, '\r');
+			//if (split != NULL) *split = 0;
+		}
+		memccpy (a, str, 0, 100);
+	}
+	else
+	{
+		fprintf (stderr, "[" __FILE__ ":%u] E! Request non supportato [E-5]\n", __LINE__);				//response 405!!!!!
+		return -5;
+	}
+
+	return 0;
+}
+
+int http_get_file (char *path, char *b)
+{
+	struct stat s;
+	FILE *f;
+	size_t r;
+	char p[HTTP_REQUEST_FILE_LEN];	
+
+	if (!_http_flag_init)
+	{
+		fprintf (stderr, "[" __FILE__ ":%u] E! Libreria non inizializzata\n", __LINE__);
+		return -1;
+	}
+
+	//Azzero i buffer
+	memset (p, 0, HTTP_REQUEST_FILE_LEN);
+
+	if (stat (path, &s))	//man 2 stat
+	{
+		if (errno == ENOENT)
+		{
+			if (path[0] == 0)
+			{
+				printf ("mi richiamo con il file scritto\n");
+				return http_get_file ("index.html", b);
+			}
+			
+			fprintf (stderr, "[" __FILE__ ":%u] E! File %s non trovato\n", __LINE__, path);
+			return 404;
+		}
+		
+		fprintf (stderr, "[" __FILE__ ":%u] E! %s\n", __LINE__, strerror(errno));
+		return -1;
+	}
+
+	//man 7 inode
+	if ((s.st_mode & 0170000) == S_IFREG)	// è un file regolare
+	{
+		;
+	}
+	else if ((s.st_mode & 0170000) == S_IFDIR)	// è una directory
+	{
+		if (_http_debug) printf ("[" __FILE__ ":%u] i! Il file %s è una cartella\n", __LINE__, path);
+		printf ("mi richiamo aggiungendo il file\n");
+
+		strcpy (p, path);
+		
+		if (p[strlen(p)-1] != '/') p[strlen(p)] = '/';
+		strcat (p, "index.html");
+		
+		if (http_get_file (p, b) == 200)
+			return 200;
+		else
+			return 0;
+	}
+	else
+	{
+		fprintf (stderr, "[" __FILE__ ":%u] E! Il file %s non è né un file regolare né una directory\n", __LINE__, path);
+		return -1;
+	}
+
+	// il file è un file regolare
+	f = fopen (path, "r");
+	if (f == NULL)
+	{
+		fprintf (stderr, "[" __FILE__ ":%u] E! %s\n", __LINE__, strerror(errno));
+		return -1;
+	}
+
+	r = fread (b, 1, HTTP_FILE_MAX_LEN, f);
+	fclose (f);
+	b[r] = 0;
+
+	if (_http_debug) printf ("[" __FILE__ ":%u] i! Letto file %s correttamente\n", __LINE__, path);
+	if (_http_debug >= 2) _http_buffer_dump (b, r);
+
+	return 200;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void _http_buffer_dump (char *b, ssize_t l)
+{
+	ssize_t i;
+
+	for (i=0; i<l; i++)
+	{
+		printf ("%02x ", *(b+i));
+	}
+	printf ("\b\n");
+}
---
+++ http.h
@@ -0,0 +1,62 @@
+#ifndef _HTTP_H
+	#define _HTTP_H
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>  	//funzione per la gestione degli errori
+	#include <sys/types.h>	//stat
+	#include <sys/stat.h>
+	#include <unistd.h>
+
+#define HTTP_REQUEST_FILE_LEN	100		// Lunghezza massima file richiesto HTTP
+#define	HTTP_FILE_MAX_LEN 		65535	// Lunghezza massima del file da inviare
+
+int _http_flag_init = 0;
+int _http_debug;
+
+/*	Inizializza la libreria
+ *	Argomenti:
+ * 		debug:	Imposta il livello di debugging
+ * 				0: Spento
+ * 				1: Visualizza messaggi
+ * 				2: Visualizza buffer
+*/
+void http_initialize(int debug);
+
+
+/*	Analizza una request HTTP compresa di header partendo da un buffer
+ *	Argomenti:
+ * 		r:		Buffer in cui e' locata la request
+ * 		t:		Buffer su cui salvare il tipo di request "GET", "POST" ...
+ * 		f:		Buffer su cui viene salvato il file richiesto dalla request "/index.html"
+ * 		a:		Buffer su cui vengono salvati gli argomenti GET o POST
+ * 	Ritorno:
+ * 		0:		request correttamente analizzata
+ * 		-1:		Request invalida: la richiesta non contiene ritorni a capo
+ * 		-2:		Request invalida: la prima riga "GET / HTTP/1.1" non contiene spazi
+ * 		-3:		Request invalida: metodo HTTP lungo piu' di 5 caratteri
+ * 		-4:		Request invalida: filename richiesto supera HTTP_REQUEST_FILE_LEN caratteri
+ *		-5:		Metodo request non supportato
+*/
+int http_parse_request (char *r, char *t, char *f, char *a);
+
+/*	Cerca e legge il file richiesto
+ * 	Argomenti:
+ * 		path:	Directory del file
+ * 		b:		Buffer di lettura del file
+ * 	Ritorno:
+ * 		200:	File regolare, aperto e letto
+ * 		404:	File non trovato
+ * 		0:		Path è una directory
+ * 		-1:		Errore
+*/
+int http_get_file (char *path, char *b);
+
+
+
+
+
+
+void _http_buffer_dump (char *b, ssize_t l);
+
+#endif
